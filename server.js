5e2d7c847d9ea18ecd31c894f0c9fb822c03f572
CalvinDT <ct223344@gmail.com>
2023-01-10 15:21:56 -0500

    Install Packages with update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
